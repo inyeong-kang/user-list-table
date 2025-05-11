fe890153fe1ea7a1cfd58951514b32d6860ed708
yeong21 <yeong21@yeong21ui-MacBookAir.local>
2025-05-11 10:01:44 +0900

    Chore: 개발 환경 구축
    - React, TypeScript 기반의 Vite 프로젝트 생성
    - AntD, styled-components 설치
    - 절대 경로 세팅

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Button } from "antd";
+import styled from "styled-components";
+import exampleImage from "@/assets/react.svg";
+
+const StyledButton = styled(Button)`
+  background-color: #e05419;
+  color: #fff;
+`;
+
+function App() {
+  return (
+    <>
+      <StyledButton type="primary">Business Canvas</StyledButton>
+      <img src={exampleImage} alt="vite" />
+    </>
+  );
+}
+
+export default App;
