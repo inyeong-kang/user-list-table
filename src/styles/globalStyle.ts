56ada3c1abdf0c53f9a3fe4ab957ecde999b73dd
inyeong-kang <tomatorkd@naver.com>
2025-05-11 18:33:35 +0900

    Feat: styles 에 font 관련 설정 추가
    - pretendard font 전역 관련 세팅 추가
    - antd 전역 theme 객체 정의
    - prettier 적용

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,15 +1,60 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
-import { reset } from "./reset";
+import { reset } from './reset';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
     
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 300;
+    font-display: swap;
+    src: url('/src/assets/fonts/Pretendard-Light.woff') format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 400;
+    font-display: swap;
+    src: url('/src/assets/fonts/Pretendard-Regular.woff') format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 500;
+    font-display: swap;
+    src: url('/src/assets/fonts/Pretendard-Medium.woff') format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-display: swap;
+    src: url('/src/assets/fonts/Pretendard-SemiBold.woff') format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 700;
+    font-display: swap;
+    src: url('/src/assets/fonts/Pretendard-Bold.woff') format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 800;
+    font-display: swap;
+    src: url('/src/assets/fonts/Pretendard-ExtraBold.woff') format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 900;
+    font-display: swap;
+    src: url('/src/assets/fonts/Pretendard-Black.woff') format('woff');
+  }
+
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    border:none
+    border: none;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
   
   ul,
@@ -19,7 +64,7 @@ export const GlobalStyle = createGlobalStyle`
   
   html,
   body {
-    font-family: sans-serif;
+    font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     font-size: 62.5%;
   }
 
@@ -36,11 +81,10 @@ export const GlobalStyle = createGlobalStyle`
     --split: rgba(0, 0, 0, 0.06);
     --alter: rgba(0, 0, 0, 0.02);
     
-    
     /* Fonts *****************************************/
-    --text-heading5: 600 16px/24px Prentarded;
-    --text-base-strong: 600 14px/22px Prentarded;
-    --text-base-normal: 400 14px/22px Prentarded;
+    --heading5: 600 16px/24px Pretendard;
+    --text-title: 600 14px/22px Pretendard;
+    --text-content: 400 14px/22px Pretendard;
 
     /* Padding *****************************************/
     --padding-xs: 8px;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,21 +1,21 @@
-import type { DefaultTheme } from "styled-components";
-import { keyframes } from "styled-components";
+import type { DefaultTheme } from 'styled-components';
+import { keyframes } from 'styled-components';
 
-const breakpoint: Record<"sm" | "md" | "lg", `${number}px`> = {
-  /** @media (min-width: 576px) { ... } */
-  sm: "576px",
-  /** @media (min-width: 768px) { ... } */
-  md: "768px",
-  /** @media (min-width: 1440px) { ... }*/
-  lg: "1440px",
+const breakpoint: Record<'sm' | 'md' | 'lg', `${number}px`> = {
+    /** @media (min-width: 576px) { ... } */
+    sm: '576px',
+    /** @media (min-width: 768px) { ... } */
+    md: '768px',
+    /** @media (min-width: 1440px) { ... }*/
+    lg: '1440px',
 };
 
 const zIndex = {
-  modal: 200,
+    modal: 200,
 };
 
 const animation = {
-  skeletonGradientWave: keyframes`
+    skeletonGradientWave: keyframes`
   to {
       background-position-x: -200%;
     }
@@ -27,7 +27,19 @@ export type Breakpoint = typeof breakpoint;
 export type Animation = typeof animation;
 
 export const theme: DefaultTheme = {
-  breakpoint,
-  zIndex,
-  animation,
+    breakpoint,
+    zIndex,
+    animation,
+};
+
+export const antdTheme = {
+    token: {
+        colorPrimary: '#4A7CFE',
+        fontFamily: 'Pretendard',
+    },
+    components: {
+        Button: {
+            borderRadius: 8,
+        },
+    },
 };
