011ae7bd8d9d567d7477149843e4db6ee7dfc943
inyeong-kang <tomatorkd@naver.com>
2025-05-11 18:36:18 +0900

    Feat: main, app.tsx 업데이트
    - app.tsx 경로명 대문자 > 소문자로 수정
    - main.tsx 에 react-query 전역 세팅 및 provider 추가
    - main.tsx 에 antd, styled-components 전역 세팅 및 provider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConfigProvider, theme } from 'antd';
+import { ThemeProvider } from 'styled-components';
+
+import App from './app';
+import { antdTheme, GlobalStyle } from './styles';
 
 async function enableMocking() {
     if (process.env.NODE_ENV !== 'development') {
@@ -11,10 +16,27 @@ async function enableMocking() {
     return worker.start();
 }
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            retry: false,
+        },
+    },
+});
+
 enableMocking().then(() => {
     ReactDOM.createRoot(document.getElementById('root')!).render(
         <React.StrictMode>
-            <App />
+            <QueryClientProvider client={queryClient}>
+                <ConfigProvider theme={antdTheme}>
+                    <ThemeProvider theme={theme}>
+                        <GlobalStyle />
+                        <App />
+                    </ThemeProvider>
+                </ConfigProvider>
+            </QueryClientProvider>
         </React.StrictMode>,
     );
 });
