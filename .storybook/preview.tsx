1b442e301a12a49525291d6b71c89d4e00cd09fe
inyeong-kang <tomatorkd@naver.com>
2025-05-12 11:14:34 +0900

    Fix: storybook 설정에 react-query provider 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,24 +1,27 @@
-import type { Preview } from "@storybook/react";
-import React from "react";
-import { GlobalStyle } from "../src/styles/globalStyle";
+import type { Preview } from '@storybook/react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import React from 'react';
+import { GlobalStyle } from '../src/styles/globalStyle';
+
+const queryClient = new QueryClient();
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
-  decorators: [
-    (Story) => (
-      <>
-        <GlobalStyle />
-        <Story />
-      </>
-    ),
-  ],
+    decorators: [
+        (Story) => (
+            <QueryClientProvider client={queryClient}>
+                <GlobalStyle />
+                <Story />
+            </QueryClientProvider>
+        ),
+    ],
 };
 
 export default preview;
